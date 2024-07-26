97898a64e4c19601bb94a56ab3368e6e2aa9e570
Duc <thienductranhuu2784@gmail.com>
2024-07-26 00:49:01 -0400

    customized slider section

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -11,6 +11,9 @@ class HomePage extends Component {
             <div>
                 <HomeHeader/>
                 <Specialty/>
+                <div style={{height: '300px'}}>
+
+                </div>
             </div>
         );
     }
--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -3,7 +3,9 @@ import { connect } from 'react-redux';
 import './Specialty.scss';
 
 import { FormattedMessage } from 'react-intl';
-
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 class Specialty extends Component {
 
@@ -14,12 +16,39 @@ class Specialty extends Component {
     }
 
     render() {
-
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            arrows: true,
+            className: "myCustomCarousel"
+          };
 
         return(
             <div className='section-specialty'>
                 <div className='specialty-content'>
-                    Specialty
+                <Slider {...settings}>
+                    <div className='img-customize'>
+                        <h3>1</h3>
+                    </div>
+                    <div className='img-customize'>
+                        <h3>2</h3>
+                    </div>
+                    <div className='img-customize'>
+                        <h3>3</h3>
+                    </div>
+                    <div className='img-customize'>
+                        <h3>4</h3>
+                    </div>
+                    <div className='img-customize'>
+                        <h3>5</h3>
+                    </div>
+                    <div className='img-customize'>
+                        <h3>6</h3>
+                    </div>
+                </Slider>
                 </div>
             </div>
         );
