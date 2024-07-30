5e2b6e3d487c89ee0b00732dbc0d93a0ec06c1eb
Duc <thienductranhuu2784@gmail.com>
2024-07-30 17:18:13 -0400

    added some sections

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HomeHeader from './HomeHeader';
 import Specialty from './Section/Specialty';
+import MedicalFacility from './Section/MedicalFacility'
+import OutStandingDoctor from './Section/OutStandingDoctor'
+import './HomePage.scss'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import HandBook from './Section/HandBook';
 
 class HomePage extends Component {
 
     render() {
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            // arrows: true,
+            className: "myCustomCarousel",
+          };
 
         return (
             <div>
                 <HomeHeader/>
-                <Specialty/>
-                <div style={{height: '300px'}}>
+                <Specialty 
+                    settings={settings}
+                />
+                <MedicalFacility 
+                    settings={settings}
+                />
+                <OutStandingDoctor
+                    settings={settings}
+                />
+                <HandBook
+                    settings={settings}
+                />
 
-                </div>
             </div>
         );
     }
--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
-import './Specialty.scss';
+
 
 // import { FormattedMessage } from 'react-intl';
 import Slider from 'react-slick';
@@ -8,43 +8,43 @@ import Slider from 'react-slick';
 // import specialtyImg from "../../../assets/specialty/co-xuong-khop.jpg"
 
   
-class Specialty extends Component {
+class HandBook extends Component {
 
 
     render() {
 
         return(
-            <div className='section-share section-specialty'>
+            <div className='section-share section-handbook'>
                 <div className='section-container'>
                     <div className='section-header'>
-                        <span className='title-section'>Chuyên khoa phổ biến</span>
+                        <span className='title-section'>Cẩm nang</span>
                         <button className='btn-section'>Xem thêm</button>
                     </div>
 
                     <div className='section-body'>
                         <Slider {...this.props.settings}>
                             <div className='section-customize'>
-                                <div className='bg-image section-specialty'></div>
+                                <div className='bg-image section-handbook'></div>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className='bg-image section-specialty'></div>
+                                <div className='bg-image section-handbook'></div>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className='bg-image section-specialty'></div>
+                                <div className='bg-image section-handbook'></div>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className='bg-image section-specialty'></div>
+                                <div className='bg-image section-handbook'></div>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className='bg-image section-specialty'></div>
+                                <div className='bg-image section-handbook'></div>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                             <div className='section-customize'>
-                                <div className='bg-image section-specialty'></div>
+                                <div className='bg-image section-handbook'></div>
                                 <div>Cơ xương khớp 1</div>
                             </div>
                         </Slider>
@@ -70,4 +70,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
+export default connect(mapStateToProps, mapDispatchToProps)(HandBook);
