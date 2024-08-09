b62a004487a1e1bd4ccedbd1c167b908a7cd5ffb
Duc <thienductranhuu2784@gmail.com>
2024-08-09 18:29:14 -0400

    save values that user gives into state

--- src/containers/System/Admin/ManageDoctor.js
+++ src/containers/System/Admin/ManageDoctor.js
@@ -7,6 +7,13 @@ import MarkdownIt from 'markdown-it';
 import MdEditor from 'react-markdown-editor-lite';
 
 import 'react-markdown-editor-lite/lib/index.css';
+import Select from 'react-select';
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
 
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
@@ -18,6 +25,7 @@ class ManageDoctor extends Component {
         this.state = {  // this here is the class TableManageUser
             contentMarkdown: '',
             contentHTML: '',
+            selectedDoctor: '',
         }
     }
 
@@ -29,18 +37,57 @@ class ManageDoctor extends Component {
 
     }
     // Finish!
-    handleEditorChange({ html, text }) {
+    handleEditorChange = ({ html, text }) => {
+    this.setState({                         //save content that user types in for doctor info into state whenever user clicks save button
+        contentMarkdown: text,
+        contentHTML: html,
+    })
     console.log('handleEditorChange', html, text);
     }
 
     handleSaveContentMarkdown = () => {
-        alert('click')
+        console.log('checkk state: ', this.state)
+    }
+
+    handleChange = (selectedOption) => {
+        console.log(selectedOption)
+        this.setState({ 
+            selectedDoctor: selectedOption
+        });
+      };
+
+    handleOnChangeDesc = (event) => {
+        this.setState({
+            description: event.target.value
+        })
     }
 
     render() {
         return (
             <div className='manage-doctor-container'>
                 <div className='manage-doctor-title'>Tạo thêm thông tin bác sĩ</div>
+                <div className='more-info'>
+                    <div className='content-left form-group'>
+                        <label>Chọn bác sĩ</label>
+                              <Select
+                                    value={this.state.selectedDoctor}
+                                    onChange={this.handleChange}
+                                    options={options}
+                                />
+                    </div>
+
+
+                    <div className='content-right'>
+                        <label>Thông tin giới thiệu: </label>
+                        <textarea 
+                            className='form-control' rows='4'
+                            onChange={ (event) => this.handleOnChangeDesc(event)}
+                            value={this.state.description}
+                        >
+    
+                        </textarea>
+                    </div>
+                </div>
                 <div className='manage-doctor-editor'>
                     <MdEditor 
                         style={{ height: '500px' }} 
