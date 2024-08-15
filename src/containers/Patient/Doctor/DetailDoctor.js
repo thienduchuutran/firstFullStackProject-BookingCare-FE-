1e052a946736d90980a758a80ad1b1b60518ee78
Duc <thienductranhuu2784@gmail.com>
2024-08-14 21:12:26 -0400

    applied dangerouslySetInnerHTML

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import HomeHeader from '../../HomePage/HomeHeader';
 import './DetailDoctor.scss'
 import { getDetailInfoDoctor } from '../../../services/userService';
+import { LANGUAGES } from '../../../utils';
 
 class DetailDoctor extends Component {
     constructor(props){
@@ -29,25 +30,37 @@ class DetailDoctor extends Component {
     }
 
 
-    render() {
+    render() {  
         // console.log(this.props.match.params.id)     //this to get the id on the URL 
         console.log('check state: ', this.state)
+        let {language} = this.props
+        let {detailDoctor} = this.state
+        let nameVi = '', nameEn = ''
+        if(detailDoctor && detailDoctor.positionData){
+            nameVi = `${detailDoctor.positionData.valueVi} ${detailDoctor.lastName} ${detailDoctor.firstName}`
+            nameEn = `${detailDoctor.positionData.valueEn} ${detailDoctor.firstName} ${detailDoctor.lastName}`
+        }
+
         return (        
             <>
                 <HomeHeader isShowBanner={false}/>
 
                 <div className='doctor-detail-container'>
                     <div className='intro-doctor'>
-                        <div className='content-left'>
+                        <div className='content-left' style={{backgroundImage: `url(${detailDoctor && detailDoctor.image? detailDoctor.image : ''})`}}>
 
                         </div>
 
                         <div className='content-right'>
                             <div className='up'>
-                                Pho giao su Le Van An
+                                {language === LANGUAGES.VI? nameVi : nameEn}
                             </div>
                             <div className='down'>
-                                djhfuhsfvscfsijfs
+                                {detailDoctor.Markdown && detailDoctor.Markdown.description
+                                && <span>
+                                    {detailDoctor.Markdown.description}
+                                </span>
+                                }
                             </div>
                         </div>
                     </div>
@@ -55,7 +68,12 @@ class DetailDoctor extends Component {
 
                     </div>
                     <div className='detail-info-doctor'>
-
+                        {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML
+                        && 
+                        <div dangerouslySetInnerHTML={{__html: detailDoctor.Markdown.contentHTML}}>
+                            
+                        </div>
+                        }
                     </div>
                     <div className='comment-doctor'>
 
@@ -68,7 +86,7 @@ class DetailDoctor extends Component {
 
 const mapStateToProps = state => {
     return {
-
+        language: state.app.language
     };
 };
 
