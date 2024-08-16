9862a260b45c7c7ce36b5ffab66b5ecab72545d0
Duc <thienductranhuu2784@gmail.com>
2024-08-15 22:25:43 -0400

    layout for scheduling appointments for doctor view, added select doctors dropdown, get list doctor data

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -1,12 +1,99 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import './ManageSchedule.scss'
+import { FormattedMessage } from 'react-intl';
+import Select from 'react-select'
+import * as actions from '../../../store/actions'
+import { CRUD_ACTIONS, LANGUAGES } from '../../../utils';
 
 class ManageSchedule extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            listDoctors: [],
+            selectedDoctor: {}
+        }
+    }
+
+    componentDidMount(){
+        this.props.fetchAllDoctors()
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        if(prevProps.allDoctors !== this.props.allDoctors){
+            let dataSelect = this.buildDataInputSelect(this.props.allDoctors)
+            this.setState({
+                listDoctors: dataSelect
+            })
+        }
+
+        // if(prevProps.language !== this.props.language){                        //need this so the options in the drop down switch from eng format to viet format whenever change language
+        //     let dataSelect = this.buildDataInputSelect(this.props.allDoctors)
+        //     this.setState({
+        //         listDoctors: dataSelect
+        //     })
+        // }
+    }
+
+
+    buildDataInputSelect = (inputData) => {
+        let result = []
+        let {language} = this.props
+        if(inputData && inputData.length> 0){
+            inputData.map((item, index) => {
+                let object = {}
+                let labelVi = `${item.lastName} ${item.firstName}`
+                let labelEn = `${item.firstName} ${item.lastName}`
+                object.label = language === LANGUAGES.VI? labelVi : labelEn
+                object.value = item.id
+                result.push(object)
+            })
+
+        }
+        return result
+    }
+
+    handleChangeSelect = async (selectedOption) => {
+        this.setState({ 
+            selectedDoctor: selectedOption
+        });
+      };
+
     render() {
+        console.log('check state: ', this.state)
         return (        //We only want the header component here because we only wanna see it in the user manage page
-            <React.Fragment>           
-                <div>
-                    manage schedule
+            <React.Fragment>   
+                <div className='manage-schedule-container'>
+                    <div className='m-s-title'>
+                        <FormattedMessage id='manage-schedule.title'/>
+                    </div>    
+
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-6 form-group'>
+                                <label>
+                                    Chọn bác sĩ
+                                </label>
+                                <Select
+                                    value={this.state.selectedDoctor}
+                                    onChange={this.handleChangeSelect}
+                                    options={this.state.listDoctors}
+                                />
+                            </div>
+                            <div className='col-6 form-group'>
+                                <label>
+                                    Chọn ngày
+                                </label>
+                                <input className='form-control'/>
+                            </div>
+
+                            <div className='col-12 pick-hour-container'>
+
+                            </div>
+
+                            <button className='btn btn-primary'> Lưu thông tin</button>
+                        </div>
+                    </div>        
                 </div>
             </React.Fragment>
         );
@@ -15,13 +102,15 @@ class ManageSchedule extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
-
+        isLoggedIn: state.user.isLoggedIn,
+        allDoctors: state.admin.allDoctors,
+        language: state.app.language
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        fetchAllDoctors: () => dispatch(actions.fetchAllDoctors()),
     };
 };
 
