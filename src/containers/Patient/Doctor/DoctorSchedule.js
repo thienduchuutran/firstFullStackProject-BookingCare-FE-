6f985ddf31501fa2a533ddd5af0ca125d7504aae
Duc <thienductranhuu2784@gmail.com>
2024-08-17 13:24:29 -0400

    displaying the weekdays options in both vietnamese and english and according format

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -22,10 +22,19 @@ class DoctorSchedule extends Component {
         console.log('moment viet: ', moment(new Date()).format('dddd - DD/MM'))                   
         console.log('moment eng: ', moment(new Date()).locale('en').format('ddd - DD/MM'))      //since we imported localization, now it prioritizes viet, so
                                                                                                 //if we wanna switch back to english, gotta use locale('en')
+        this.setArrays(language)
+    }
+
+    //doing this so it looks clean in componentDidMount and componentDidUpdate
+    setArrays = (language) => {
         let allDays = []
         for (let i = 0; i < 8 ; i++){
             let object = {}
-            object.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM')         //new Date() is today, inside the format() we have dddd to display "thứ"
+            if(language === LANGUAGES.VI){
+                object.label = moment(new Date()).add(i, 'days').format('dddd - DD/MM')         //new Date() is today, inside the format() we have dddd to display "thứ"
+            }else{
+                object.label = moment(new Date()).add(i, 'days').locale('en').format('ddd - MM/DD')
+            }
             object.value = moment(new Date()).add(i, 'days').startOf('day').valueOf()       //add(i, 'days') to go through all weekdays, startOf('day') is
                                                                                             // getting the first hour of the day, valueOf() converting to Unix time in milliseconds
             allDays.push(object)
@@ -36,12 +45,15 @@ class DoctorSchedule extends Component {
         })
     }
 
-    componentDidUpdate(prevProps, prevState, snapshot){
-
+    componentDidUpdate(prevProps, prevState, snapshot){                             //gotta run again in the componentDidUpdate() since it's gonna keep running 
+        if(this.props.language !== prevProps.language){
+            this.setArrays(this.props.language)
+        }            
     }
+    
 
 
-    render() {  
+    render(){  
         // let options = [
         //     {label: 'Thứ 2', value: '2'},
         //     {label: 'Thứ 2', value: '2'},
