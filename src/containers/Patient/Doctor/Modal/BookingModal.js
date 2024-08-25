8f3021b07a38c2380a5686634abf00abf2276ce8
Duc <thienductranhuu2784@gmail.com>
2024-08-25 08:28:53 -0400

    coverting timeType to send to backend

--- src/containers/Patient/Doctor/Modal/BookingModal.js
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -11,6 +11,7 @@ import { LANGUAGES } from '../../../../utils';
 import Select from 'react-select'
 import { postPatientBookAppointment } from '../../../../services/userService';
 import { toast } from 'react-toastify';
+import moment, { lang } from 'moment';
 
 
 class BookingModal extends Component {
@@ -102,6 +103,8 @@ class BookingModal extends Component {
         //validate input
 
         let date = new Date(this.state.birthday).getTime()  //this getTime() is converting the timestamp from javascript type to unix string
+        
+        let timeString = this.buildTimeBooking(this.props.dataTime)
         let res = await postPatientBookAppointment({
             fullName: this.state.fullName,
             phoneNumber: this.state.phoneNumber,
@@ -111,7 +114,9 @@ class BookingModal extends Component {
             date: date,
             selectedGender: this.state.selectedGender.value,
             doctorId: this.state.doctorId,
-            timeType: this.state.timeType
+            timeType: this.state.timeType,
+            language: this.props.language,
+            timeString: timeString
         })
 
         if(res && res.errCode === 0){
@@ -122,6 +127,23 @@ class BookingModal extends Component {
         }
 
     }
+
+    buildTimeBooking = (dataTime) => {
+        let {language} = this.props
+        if(dataTime && !_.isEmpty(dataTime)){
+            let time = language === LANGUAGES.VI ?
+            dataTime.timeTypeData.valueVi : dataTime.timeTypeData.valueEn
+
+            let date = language === LANGUAGES.VI ? 
+                moment.unix(+dataTime.date / 1000).format('dddd - DD/MM/YYYY').toUpperCase() //converting a string to JS date type
+                :
+                moment.unix(+dataTime.date / 1000).locale('en').format('ddd - MM/DD/YYYY')   //the plus sign is to convert from string to integer then divide by 1000 to switch from milliseconds to seconds
+                
+                return `${time} - ${date}`
+        }
+        return ``
+    }
+
     render(){ 
         let {isOpenModal, closeBookingModal, dataTime} = this.props
         let doctorId = ''
