a169f5c235e948871b56828b4e11392747e98ef7
Duc <thienductranhuu2784@gmail.com>
2024-08-26 18:56:32 -0400

    defining a new route to lead to a new page-the verify booking page

---
+++ src/containers/Patient/VerifyEmail.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { FormattedMessage } from 'react-intl';
+
+
+
+class VerifyEmail extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        }
+    }
+
+    async componentDidMount(){
+
+    }
+
+
+    async componentDidUpdate(prevProps, prevState, snapshot){                 //gotta run again in the componentDidUpdate() since it's gonna keep running 
+        if(this.props.language !== prevProps.language){
+
+        }  
+         
+    }
+
+    render(){ 
+
+        return (    
+            <> 
+            <div>hello from verufy</div>
+            </>   
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(VerifyEmail);
