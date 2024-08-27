329b050a95ec87cbb34480414fab533e66b961c9
Duc <thienductranhuu2784@gmail.com>
2024-08-26 23:45:55 -0400

    dynamically load specialties data from db into homepage

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
 import './Specialty.scss';
+import { getAllSpecialty } from '../../../services/userService';
 
 // import { FormattedMessage } from 'react-intl';
 import Slider from 'react-slick';
+import { FormattedMessage } from 'react-intl';
 
 // import specialtyImg from "../../../assets/specialty/co-xuong-khop.jpg"
 
   
 class Specialty extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            dataSpecialty: []
+        }
+    }
 
+    async componentDidMount(){
+        let res = await getAllSpecialty()
+        if(res && res.errCode === 0){
+            this.setState({
+                dataSpecialty: res.data ? res.data : []
+            })
+        }
+    }
 
     render() {
-
+        let {dataSpecialty} = this.state
         return(
             <div className='section-share section-specialty'>
                 <div className='section-container'>
                     <div className='section-header'>
-                        <span className='title-section'>Chuyên khoa phổ biến</span>
-                        <button className='btn-section'>Xem thêm</button>
+                        <span className='title-section'><FormattedMessage id='homepage.specialty-popular'/></span>
+                        <button className='btn-section'><FormattedMessage id='homepage.more-info'/></button>
                     </div>
 
                     <div className='section-body'>
                         <Slider {...this.props.settings}>
-                            <div className='section-customize'>
-                                <div className='bg-image section-specialty'></div>
-                                <div>Cơ xương khớp 1</div>
-                            </div>
+                            {dataSpecialty && dataSpecialty.length > 0 &&
+                            dataSpecialty.map((item, index)=> {
+                                return (
+                                    <div className='section-customize specialty-child' key={index}>
+                                    <div className='bg-image section-specialty'
+                                        style={{backgroundImage: `url(${item.image})`}}
+                                    >
 
+                                    </div>
+                                    <div className='specialty-name'>{item.name}</div>
+                                </div>
+                                )
+                            })}
                         </Slider>
                     </div>
 
